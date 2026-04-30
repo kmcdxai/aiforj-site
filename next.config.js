60a82a88975f9f22800e180bb63bd1710a7af477
Kevin Cooke <kevincooke@Kevins-MacBook-Air.local>
2026-04-30 14:25:35 -0400

    Add final exam mastery page

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/final',
+        destination: '/final.html',
+      },
+    ];
+  },
   async redirects() {
     return [
       {
